f9935e5f6b9faaf55fda5abafa64f47cab649442
Programming-Daniel <111247505+Programming-Daniel@users.noreply.github.com>
2022-09-02 21:55:43 -0500

    Add files via upload

--- snake_game.py
+++ snake_game.py
@@ -1,34 +1,83 @@
-#Estado inicial 
+#librerías necesarias
+from tkinter import font
+import keyboard,pygame, sys, time, random
+
+#Estado inicial
+pygame.init()
+superficie_juego= pygame.display.set_mode((500,500))
 print("Bienvenidos al juego de la serpiente, usa las flechas para moverte")
 print("presione una flecha para empezar")
 
-#coordenadas iniciales
-import random
-snake_coordinates = (9,9)
-apple_coordinates = random.randint(1,18)
-#Mostrar puntajes
-snake_size = (2)
-puntaje = 0
+#coordenadas y valores iniciales
+def posicion_fruta():
+
+    posicion_random = random.randint (0,49)*10
+    pos_fruta = [posicion_random, posicion_random]
+    return pos_fruta
+
+#contador
+def contador():
+    puntuacion_ini = 0
+    puntuacion_ini = puntuacion_ini+1
+    print(puntuacion_ini)
 
 
-#contador y tamaño de la serpiente
-if snake_coordinates == apple_coordinates:
-    puntaje + 1
-    snake_size + 1
-print(puntaje)
+def principal():
 
-    
+    snake_coordinates =[200,100]
+    snake_body = [[100,50],[90,50],[80,50]]
+    pos_fruta = posicion_fruta()
+    cambio = "RIGHT"
+    run = True
 
-#Movimiento de la serpiente (Aún no lo descifro)
-import keyboard
-from pygame import K_LEFT, K_UP, K_DOWN, K_RIGHT
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    cambio = "RIGHT"
+                if event.key == pygame.K_LEFT:
+                    cambio = "LEFT"
+                if event.key == pygame.K_UP:
+                    cambio = "UP"
+                if event.key == pygame.K_DOWN:
+                    cambio = "DOWN"
+        if cambio == "RIGHT":
+            snake_coordinates[0] += 10
+        if cambio == "LEFT":
+            snake_coordinates[0] -= 10
+        if cambio == "UP":
+            snake_coordinates[0] -= 10
+        if cambio == "DOWN":
+            snake_coordinates[0] += 10
 
-while True:
-    if keyboard.read_key(K_LEFT):
-        print("presionaste flecha izquierda")
-    elif keyboard.read_key(K_RIGHT):
-        print("presionaste flecha derecha")
-        
+        snake_body.insert(0, list(snake_coordinates))
+
+        if snake_coordinates == pos_fruta:
+            pos_fruta = posicion_fruta()
+            contador()
+        else:
+            snake_body.pop()
+
+        cabeza = snake_body[-1]
+        for i in range(len(snake_body) - 1):
+            part = snake_body[i]
+            if cabeza[0] == part[0] and cabeza[1] == part[1]:
+                run = False
+                print("Perdiste")
+
+        superficie_juego.fill((0,0,0))
+
+        for pos in snake_body:
+            pygame.draw.rect(superficie_juego,(200,200,200), pygame.Rect(pos[0], pos[1], 10, 10))
+
+        pygame.draw.rect(superficie_juego,(169,6,6), pygame.Rect(pos_fruta[0], pos_fruta[1], 10, 10))
+
+        pygame.display.flip()
+
+principal()
+
+pygame.quit()
 
 
-    
