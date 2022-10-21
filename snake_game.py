41a9d3c86bd2d66137f9dcf8a4db4ffbd2a7397f
Programming-Daniel <111247505+Programming-Daniel@users.noreply.github.com>
2022-10-20 23:47:18 -0500

    Add files via upload

--- snake_game.py
+++ snake_game.py
@@ -1,93 +1,57 @@
-#hay que correr "pip install pygame" y "pip install replit" en la consola para poder importar pygame, 
+"""
+Proyecto Snake Game python
+El programa simula lo que es el juego de la serpiente de los antiguos teléfonos.
+El programa usa la interfaz gráfica de pygame para renderizar el cuerpo de la serpiente y la manzana,
+para moverse hay que usar las flechas.
+"""
+
 #librerías necesarias
 import pygame, time, replit,random, sys
 
-color_s = input("Elige el color de tu serpiente (RED,GREEN,BLUE,GRAY,WHITE)")
-color_m = input("Elige el color de la manzana (RED,GREEN,BLUE,GRAY,WHITE)")
-
-
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-GRAY = (200, 200, 200)
-PERRY = (72, 201, 176)
-CLEARGRAY = (229, 232, 232)
-WHITE = (244, 246, 247)
-fps = pygame.time.Clock()
-
+"""
+Esta función inicializa los módulos importados de pygame
+"""
 pygame.init()
 
-superficie_juego = pygame.display.set_mode((500,500))
-pygame.display.set_caption("Menú")
 
-font2 = pygame.font.SysFont(None, 30)
-
-text = font2.render("Bienvenido a Snake game", True, RED)
-rect = text.get_rect()
-
-instru = font2.render("Dentro del juego", True, RED)
-
-instru2 = font2.render("Utiliza las flechas para moverte", True, RED)
-
-instru3 = font2.render("Presiona espacio para empezar", True, WHITE)
-rect = instru3.get_rect()
-pygame.draw.rect(instru3, GRAY, rect, 1)
-
-puntuacion_prev = font2.render("Puntuación previa:", True, PERRY)
-rect = puntuacion_prev.get_rect()
-pygame.draw.rect(puntuacion_prev, GRAY, rect, 1)
-
-font = pygame.font.Font(None, 30)
-
-fps = pygame.time.Clock()
-
+"""
+================== funciones del juego  =====================================
+"""
 
 def posicion_fruta():
-
+    """
+    (uso de funciones, uso de operadores, uso de lisas)
+    genera una lista de dos valores 
+    devuelve: una coordenada aleatoria en x,y
+    """
     posicion_random = random.randint (0,49)*10
     pos_fruta = [posicion_random, posicion_random]
     return pos_fruta
 
 def puntuacion(score):
+    """
+    (Uso de fuciones, uso de operadores)
+    recibe; score (valor numérico)
+    Lo usa como un acumulador y le va sumano 1 
+    cada vez que la serpiente se coma una manzana
+    devuelve: la puntación del usuario
+    """
     score += 1
     return score
 
-def loading(percent):
-    for i in range(100):
-        replit.clear()
-        percent +=1
-        print("""Snake game\nEstatus: cargando %"""+ str(percent)
-            )
-        time.sleep(.03)
-  
-    print("!Carga completa!\n")
-    time.sleep(.5)
-    return percent
-
-def cuadricula(superficie_juego):
-    square = 10
-    for i in range(0, 500, square):
-        for j in range(0, 500, square):
-            grid = pygame.Rect(i, j, square, square)
-    pygame.draw.rect(superficie_juego, CLEARGRAY, grid, 1)
-
-
-def principal():
-
-    valores_vel=[[15,20],[25,30]]
-    percent = 0
-    snake_coordinates =[200,100]
-    snake_body = [[100,50],[90,50],[80,50]]
-    pos_fruta = posicion_fruta()
-    cambio = "RIGHT"
-    run = True
-    score = 0
-
-    loading(percent)
+def principal(valores_vel, snake_coordinates, cuerpo, pos_fruta, cambio, run, score):
+    """
+    (Uso de operadores, funciones, listas, matrices, ciclos, ciclos anidados, condicionales, condicionales anidados, boleanos)
+    recibe: valores_vel (matriz de valores numéricos), snake_coordinates (lista de valores numéricos), 
+    pos_fruta (variable que manda a llamar una función), cambio (Valor de tipo cadena), run (Valor de tipo boleano) y 
+    score (valor numérico)
+    Define el funcionamiento del juego y movimiento de la serpiente, así como también renderiza en pantalla la puntuación 
+    del usuario y a la serpiente
+    Al perder: manda a llamar la función para reegresar al menú, y entra a la función como parámetro 
+    la puntuación previa obtenida
+    """
 
     while run:
-        cuadricula(superficie_juego)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit
@@ -110,7 +74,8 @@ def principal():
         elif cambio == "DOWN":
             snake_coordinates[1] += 10
 
-        snake_body.insert(0, list(snake_coordinates))
+        cuerpo.insert(0, list(snake_coordinates))
+
         if score < 5:
             fps.tick(valores_vel[0][0])
         elif score >=5 and score<10:
@@ -124,11 +89,11 @@ def principal():
             pos_fruta = posicion_fruta()
             score = puntuacion(score)
         else:
-            snake_body.pop()
+            cuerpo.pop()
 
-        cabeza = snake_body[-1]
-        for i in range(len(snake_body) - 1):
-            part = snake_body[i]
+        cabeza = cuerpo[-1]
+        for i in range(len(cuerpo) - 1):
+            part = cuerpo[i]
             if cabeza[0] == part[0] and cabeza[1] == part[1]:
                 run = False
                 print("PERDISTE")
@@ -136,10 +101,10 @@ def principal():
                 
         superficie_juego.fill((0,0,0))
 
-        for pos in snake_body:
-            pygame.draw.rect(superficie_juego,color_s, pygame.Rect(pos[0], pos[1], 10, 10))
+        for pos in cuerpo:
+            pygame.draw.rect(superficie_juego,(255,0,0), pygame.Rect(pos[0], pos[1], 10, 10))
 
-        pygame.draw.rect(superficie_juego,color_m, pygame.Rect(pos_fruta[0], pos_fruta[1], 10, 10))
+        pygame.draw.rect(superficie_juego,(255,0,0), pygame.Rect(pos_fruta[0], pos_fruta[1], 10, 10))
 
         text = font.render(str(score),0,PERRY)
         superficie_juego.blit(text, (480,20))
@@ -157,6 +122,12 @@ def principal():
         pygame.display.flip()
 
 def menu(superficie_juego, text, instru, instru2, instru3):
+    """
+    (Uso de fuciones, listas, condicionales, condicionales anidados, ciclos, ciclos anidados, boleanos)
+    recibe: superficie_juego (funcion con una lista), text, instru, instru2, 
+    instru3 (funciones de renderizado con listas de valores)
+    Al terminar: manda a llamar la función principal para iniciar el juego
+    """
     run2 = True
     background = BLACK
 
@@ -177,10 +148,55 @@ def menu(superficie_juego, text, instru, instru2, instru3):
         superficie_juego.blit(puntuacion_prev, (20, 20)) 
         pygame.display.update()
 
-    principal()
+    principal(valores_vel,snake_coordinates, cuerpo, pos_fruta, cambio, run, score)
 
+"""
+========  parte principal del programa ========================================
+"""
+
+#Se definen variables esenciales para el color y para la velocidad de la serpiente
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GRAY = (200, 200, 200)
+PERRY = (72, 201, 176)
+WHITE = (244, 246, 247)
+
+fps = pygame.time.Clock()
+
+#Se definen las dimensiones de la ventana donde se renderiza el juego
+superficie_juego = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Snake Game")
+
+#Se definen las fuentes que se usarán para el texto renderizado
+font = pygame.font.Font(None, 30)
+font2 = pygame.font.SysFont(None, 30)
+
+#Renderizado de las instrucciones y mensaje de bienvenida
+text = font2.render("Bienvenido a Snake game", True, RED)
+rect = text.get_rect()
+
+instru = font2.render("Dentro del juego", True, RED)
+
+instru2 = font2.render("Utiliza las flechas para moverte", True, RED)
+
+instru3 = font2.render("Presiona espacio para empezar", True, WHITE)
+rect = instru3.get_rect()
+pygame.draw.rect(instru3, GRAY, rect, 1)
+
+puntuacion_prev = font2.render("Puntuación previa:", True, PERRY)
+rect = puntuacion_prev.get_rect()
+pygame.draw.rect(puntuacion_prev, GRAY, rect, 1)
+
+#Variables esenciales para el funcionamiento y coordenadas
+valores_vel=[[15,20],[25,30]]
+snake_coordinates =[200,100]
+cuerpo = [[100,50],[90,50],[80,50]]
+pos_fruta = posicion_fruta()
+cambio = "RIGHT"
+run = True
+score = 0
 
 menu(superficie_juego, text, instru, instru2, instru3)
 
-pygame.quit
+pygame.quit()
 
